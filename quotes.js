20b955980db098df7ce4d3278ad29e8266454403
Pankaj Adhikari <pankajadhikari809@gmail.com>
2024-02-29 14:10:34 +0545

    responsive

--- quotes.js
+++ quotes.js
@@ -1,13 +1,17 @@
 const quotes = {
-    science: [
-      "The good thing about science is that it's true whether or not you believe in it. - Neil deGrasse Tyson",
-      "Science is not only a disciple of reason but, also, one of romance and passion. - Stephen Hawking",
-      "The most exciting phrase to hear in science, the one that heralds new discoveries, is not 'Eureka!' but 'That's funny...' - Isaac Asimov"
-    ],
-    inspiration: [
-      "The only way to do great work is to love what you do. - Steve Jobs",
-      "Believe you can and you're halfway there. - Theodore Roosevelt",
-      "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt"
-    ]
-  };
-  
+  science: [
+    "Science is a way of thinking much more than it is a body of knowledge. - Carl Sagan",
+    "The most beautiful thing we can experience is the mysterious. It is the source of all true art and science. - Albert Einstein",
+    "The important thing is not to stop questioning. Curiosity has its own reason for existence. - Albert Einstein",
+    "The science of today is the technology of tomorrow. - Edward Teller",
+    "Science knows no country, because knowledge belongs to humanity, and is the torch which illuminates the world. - Louis Pasteur",
+
+  ],
+  inspiration: [
+    "Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill",
+    "Believe you can and you're halfway there. - Theodore Roosevelt",
+    "The only way to do great work is to love what you do. - Steve Jobs",
+    "The future belongs to those who believe in the beauty of their dreams. - Eleanor Roosevelt",
+    "You are never too old to set another goal or to dream a new dream. - C.S. Lewis",
+  ]
+};
